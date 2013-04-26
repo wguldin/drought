2b14276e4608ae5e86aa356b84bd9fdfedb30130
EmptyCan <wguldin@gmail.com>
2013-04-26 16:25:15 -0500

    Website view is working!
    Took out duplicate references to template_dir and static_root.

--- drought/settings.py
+++ drought/settings.py
@@ -78,7 +78,6 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -123,13 +122,6 @@ ROOT_URLCONF = 'drought.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'drought.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
