56e66263d88f2f287d3f0de1e2d464e0dfe21243
EmptyCan <wguldin@gmail.com>
2013-05-01 10:12:02 -0500

    Fixed date in SQLite database
    The date still isn't showing up in the django web interface (admin or
    index)

--- drought/data/models.py
+++ drought/data/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Drought(models.Model):
 
 	rowid = models.CharField(max_length=12, primary_key=True)
-	date = models.DateTimeField()
-	#none = models.FloatField(blank=True, null=True)
-	#D0D4 = models.FloatField(blank=True, null=True)
-	#D1D4 = models.FloatField(blank=True, null=True)
-	#D2D4 = models.FloatField(blank=True, null=True)
+	date = models.DateTimeField(blank=True, null=True)
+	none = models.FloatField(blank=True, null=True)
+	D0D4 = models.FloatField(blank=True, null=True)
+	D1D4 = models.FloatField(blank=True, null=True)
+	D2D4 = models.FloatField(blank=True, null=True)
 	D3D4 = models.FloatField(blank=True, null=True)
-	#D4 = models.FloatField(blank=True, null=True)
+	D4 = models.FloatField(blank=True, null=True)
 
 	class Meta:
 		db_table = 'drought_data'
